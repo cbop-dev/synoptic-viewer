ae684815c849bae8dc320eac6b2159c7afd02d35
cbop-dev <ban33man-dev@yahoo.com>
2026-09-16 17:06:07 -0400

    again...

--- src/lib/components/content/CustomParallelViewer.svelte
+++ src/lib/components/content/CustomParallelViewer.svelte
@@ -667,7 +667,7 @@
 
 		opt.request.tab = 1;
 
-		indow.location.protocol + '//' + window.location.host + window.location.pathname + '/';
+		indow.location.protocol + '//' + window.location.host + window.location.pathname;
 		return baseurl + opt.generateURI();
 	}
 
